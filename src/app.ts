f581ef4c73cf5870d2b8885c6c04f4fe5c51e6af
unknown <wax.fabian@gmail.com>
2022-11-14 17:50:15 +0100

    Removes unneeded code

--- src/app.ts
+++ src/app.ts
@@ -14,19 +14,6 @@ let filtered8 = Puzzles.puzzles.filter( (puzzle) => {
     if(puzzle.keyRange.startsWith('8')) return puzzle;
 })
 
-filtered1.forEach(puzzle => {
-    puzzle.privateKey = puzzle.privateKey.replace('0', '')
-})
-filtered2.forEach(puzzle => {
-    puzzle.privateKey = puzzle.privateKey.replace('0', '')
-})
-filtered4.forEach(puzzle => {
-    puzzle.privateKey = puzzle.privateKey.replace('0', '')
-})
-filtered8.forEach(puzzle => {
-    puzzle.privateKey = puzzle.privateKey.replace('0', '')
-})
-
 console.table(filtered1)
 console.table(filtered2)
 console.table(filtered4)
